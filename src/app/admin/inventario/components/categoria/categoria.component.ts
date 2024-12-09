ba909200b593a57fee8e4083f04f199769771bfb
unknown <lpa.carrillo@yavirac.edu.ec>
2024-12-08 19:49:10 -0500

    clase 15

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoriaService } from '../../service/categoria.service';
+
+interface Categoria {
+  id?: number;
+  nombre: string;
+  detalle?: string;
+}
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit{
 
+  private categoriaService = inject(CategoriaService)
+
+  categorias: Categoria[]=[]
+
+  ngOnInit(): void {
+    this.getCategorias() 
+  }
+
+  getCategorias(){
+    this.categoriaService.funListar().subscribe(
+      (res:any)=>{
+        this.categorias=res;
+      },
+      (error:any)=>{
+        console.log(error)
+      }
+    )
+  }
 }
--- src/app/admin/inventario/inventario.module.ts
+++ src/app/admin/inventario/inventario.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductoComponent } from './components/producto/producto.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 
 
@@ -11,7 +15,11 @@ import { CategoriaComponent } from './components/categoria/categoria.component';
     CategoriaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    TableModule,
+    DialogModule,
+    ButtonModule
   ]
 })
 export class InventarioModule { }
